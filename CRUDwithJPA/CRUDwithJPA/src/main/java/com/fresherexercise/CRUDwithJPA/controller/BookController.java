2f31b3460d5b07d962c6870d88fdb735d6c4eaf6
nhhuy-05 <huuhuy5208@gmail.com>
2022-04-01 15:04:10 +0700

    update bookcontroller

--- CRUDwithJPA/CRUDwithJPA/src/main/java/com/fresherexercise/CRUDwithJPA/controller/BookController.java
+++ CRUDwithJPA/CRUDwithJPA/src/main/java/com/fresherexercise/CRUDwithJPA/controller/BookController.java
@@ -9,6 +9,12 @@ import com.fresherexercise.CRUDwithJPA.repository.BookRepository;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -31,7 +37,7 @@ public class BookController {
 
     @Autowired
     BookRepository bookRepository;
-    
+
     @GetMapping("/getAllBook")
     public ResponseEntity<List<Book>> getAllBook(@RequestParam(required = false) String title) {
         try {
@@ -50,7 +56,7 @@ public class BookController {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-    
+
     @GetMapping("/getBookById/{id}")
     public ResponseEntity<Book> getBookById(@PathVariable("id") long id) {
         Optional<Book> authorData = bookRepository.findById(id);
@@ -90,10 +96,58 @@ public class BookController {
     public ResponseEntity<Book> addBook(@RequestBody Book book) {
         try {
             Book _book = bookRepository
-                    .save(new Book(book.getName(),book.getAuthor(),book.getType(),book.getDescription()));
+                    .save(new Book(book.getName(), book.getAuthor(), book.getType(), book.getDescription()));
             return new ResponseEntity<>(_book, HttpStatus.CREATED);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BookController.class);
+    private static SessionFactory sessionFactory;
+
+    private Session openSession() {
+        Session session = this.sessionFactory.openSession();
+        return session;
+    }
+
+    private void closeSession(Session session) {
+        if (session.isOpen()) {
+            session.disconnect();
+            session.close();
+        }
+    }
+
+    @GetMapping("/getNumberOfBook/{author}")
+    public int getNumberOfBook(@PathVariable("author") String nameAuthor) {
+        Session session = openSession();
+        Object result = null;
+        try {
+            Query query = session.createNativeQuery("SELECT COUNT(*) FROM book JOIN author ON book.author_id = author.id WHERE author.name LIKE '?'");
+            query.setParameter(1, nameAuthor);
+            result = query.getResultList();
+        } catch (NoResultException ex) {
+            LOGGER.error(ex.toString());
+        } finally {
+            closeSession(session);
+        }
+        return -1;
+    }
+    
+    @GetMapping("/findBookByName/{bookName}")
+    public Book findBookByName(@PathVariable("bookName") String bookName) {
+        Session session = openSession();
+        Object result = null;
+        try {
+            Query query = session.createNativeQuery("SELECT * FROM book WHERE book.name LIKE '%?%'", Book.class);
+            query.setParameter(1, bookName);
+            result = query.getSingleResult();
+        }catch(NoResultException ex) {
+            LOGGER.error(ex.toString());
+        } finally {
+            closeSession(session);
+        }
+        return result != null ? (Book) result : null;
+    }
+    
 }
