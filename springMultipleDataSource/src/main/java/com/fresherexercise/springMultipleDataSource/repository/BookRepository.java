5e5b3c01ad3670590beeaddc92c4a88424d063bc
Huy Nguyen <huuhuy5208@gmail.com>
2022-04-19 11:10:10 +0700

    new springMultipleDataSource

---
+++ springMultipleDataSource/src/main/java/com/fresherexercise/springMultipleDataSource/repository/BookRepository.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.fresherexercise.springMultipleDataSource.repository;
+
+import com.fresherexercise.springMultipleDataSource.book.repository.*;
+import com.fresherexercise.springMultipleDataSource.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Admin
+ */
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long>{
+    
+}
